39a400814bf52a1bca01e119defe045f3ab20ca2
ykadir42 <ykadir42@gmail.com>
2018-02-03 01:27:53 -0500

    wow

--- pringle.c
+++ pringle.c
@@ -1,32 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <math.h>
 #include <fcntl.h>
 
 int main(){
-  int pixels = 500;
+	int pixels = 600;
 
-  unsigned char pic[pixels][pixels][3];
+	unsigned char pic[pixels][pixels][3];
 
-  int i, j, k;
-  
-  for(i = 0; i < pixels; i++){
-    for(j = 0; i < pixels; i++){
-      for(k = 0; i < 3; i++){
-	pic[i][j][k] = 0;
-      }
-    }
-  }
+	int i, j, k;
 
-  int fd = open("pringle.ppm", O_WRONLY | O_CREAT, 0644);
-  for(i = 0; i < pixels; i++){
-    for(j = 0; i < pixels; i++){
-      for(k = 0; i < 3; i++){
-	write(fd, pic[i][j][k], sizeof(unsigned char));
-      }
-    }
-  }
-  close(fd);
-  
-  return 0;
+	for(i = 0; i < pixels; i++){
+		for(j = 0; j < pixels; j++){
+			pic[i][j][0] = 0;
+			pic[i][j][1] = 0;
+			pic[i][j][2] = 0;
+		}
+	}
+
+	float x, y;
+	for(x = -2; x < 2; x += 0.01){
+		for(y = -2; y < 2; y +=0.01){
+			float z = x * x - y * y;
+			float partial_x = 2 * x;
+			float partial_y = -2 * y;
+			int column = (int)(pixels / 2 + y * 5 * pixels / 48 - x * 7 * pixels / 48);
+			int row = (int)(pixels / 2 - z * pixels / 20 + y * pixels / 16 + x * pixels / 16);
+			unsigned char color_scalar = (unsigned char) 16 * -1 * (partial_y) + 128;
+			pic[row][column][0] = color_scalar;
+			pic[row][column][1] = color_scalar;
+			pic[row][column][2] = color_scalar / 4;
+
+			//rudimentary smoothing
+			if(column > 0){
+				pic[row][column - 1][0] = color_scalar;
+				pic[row][column - 1][1] = color_scalar;
+				pic[row][column - 1][2] = color_scalar / 4;
+			}
+			if(row > 0){
+				pic[row - 1][column][0] = color_scalar;
+				pic[row - 1][column][1] = color_scalar;
+				pic[row - 1][column][2] = color_scalar / 4;
+			}
+			if(column < pixels - 1){
+				pic[row][column + 1][0] = color_scalar;
+				pic[row][column + 1][1] = color_scalar;
+				pic[row][column + 1][2] = color_scalar / 4;
+			}
+			if(row < pixels - 1){
+				pic[row + 1][column][0] = color_scalar;
+				pic[row + 1][column][1] = color_scalar;
+				pic[row + 1][column][2] = color_scalar / 4;
+			}
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+	int fd = open("pringle.ppm", O_WRONLY | O_CREAT, 0644);
+	char s[100];
+	sprintf(s, "P3\n%d %d \n255\n", pixels, pixels);
+	write(fd, s, strlen(s));
+	for(i = 0; i < pixels; i++){
+		for(j = 0; j < pixels; j++){
+			for(k = 0; k < 3; k++){
+				char c[4];
+				sprintf(c, "%03d", pic[i][j][k]);
+				write(fd, c, 3*sizeof(char));
+				write(fd, " ", sizeof(" "));
+			}
+			write(fd, "\n", sizeof("\n"));
+		}
+	}
+	close(fd);
+
+	return 0;
 }
