f070895c8cd7f0614bcea4072a279584b62433c0
ykadir42 <ykadir42@gmail.com>
2018-02-03 23:58:22 -0500

    T A S T Y

--- pringle.c
+++ pringle.c
@@ -13,53 +13,56 @@ int main(){
 
 	int i, j, k;
 
-	//set background to black
+	//set background to white
 	for(i = 0; i < pixels; i++){
 		for(j = 0; j < pixels; j++){
-			pic[i][j][0] = 0;
-			pic[i][j][1] = 0;
-			pic[i][j][2] = 0;
+			pic[i][j][0] = 255;
+			pic[i][j][1] = 255;
+			pic[i][j][2] = 255;
 		}
 	}
 
 	float x, y;
 	for(x = -2; x < 2; x += 0.01){
 		for(y = -2; y < 2; y +=0.01){
-			float z = x * x - y * y;
-			float partial_y = -2 * y;
+			if(x * x * x * x / 16 + y * y * y * y / 8 < 1){
+				float z = 3 * (x * x - y * y) / 4;
+				float partial_x = -2 * x;
+				float partial_y = -2 * y;
 
-			//finding the position in the 2D picture given 3D coordinates.
-			//SLightly rotated counterclockwise for a better view.
-			int column = (int)(pixels / 2 + y * 5 * pixels / 48 - x * 7 * pixels / 48);
-			int row = (int)(pixels / 2 - z * pixels / 20 + y * pixels / 16 + x * pixels / 16);
+				//finding the position in the 2D picture given 3D coordinates.
+				//Slightly rotated counterclockwise for a better view.
+				int column = (int)(pixels / 2 + y * 4 * pixels / 48 - x * 8 * pixels / 48);
+				int row = (int)(pixels / 2 - z * pixels / 20 + y * pixels / 16 + x * pixels / 16);
 
-			//Made color proportional to the dot product of the gradient and the negative y unit vector,
-			//creating an effect as if light was coming from the positive y and z direction.
-			unsigned char color_scalar = (unsigned char) 16 * -1 * (partial_y) + 128;
-			pic[row][column][0] = color_scalar;
-			pic[row][column][1] = color_scalar;
-			pic[row][column][2] = color_scalar / 4;
+				//Made color proportional to the dot product of the gradient and a vector facing away,
+				//creating an effect as if light was coming from the positive x, y, and z direction.
+				unsigned char color_scalar = (unsigned char) (16/3 * (-2 * partial_y + partial_x) + 192);
+				pic[row][column][0] = color_scalar;
+				pic[row][column][1] = color_scalar;
+				pic[row][column][2] = color_scalar / 2;
 
-			//rudimentary smoothing
-			if(column > 0){
-				pic[row][column - 1][0] = color_scalar;
-				pic[row][column - 1][1] = color_scalar;
-				pic[row][column - 1][2] = color_scalar / 4;
-			}
-			if(row > 0){
-				pic[row - 1][column][0] = color_scalar;
-				pic[row - 1][column][1] = color_scalar;
-				pic[row - 1][column][2] = color_scalar / 4;
-			}
-			if(column < pixels - 1){
-				pic[row][column + 1][0] = color_scalar;
-				pic[row][column + 1][1] = color_scalar;
-				pic[row][column + 1][2] = color_scalar / 4;
-			}
-			if(row < pixels - 1){
-				pic[row + 1][column][0] = color_scalar;
-				pic[row + 1][column][1] = color_scalar;
-				pic[row + 1][column][2] = color_scalar / 4;
+				//rudimentary smoothing
+				if(column > 0){
+					pic[row][column - 1][0] = color_scalar;
+					pic[row][column - 1][1] = color_scalar;
+					pic[row][column - 1][2] = color_scalar / 2;
+				}
+				if(row > 0){
+					pic[row - 1][column][0] = color_scalar;
+					pic[row - 1][column][1] = color_scalar;
+					pic[row - 1][column][2] = color_scalar / 2;
+				}
+				if(column < pixels - 1){
+					pic[row][column + 1][0] = color_scalar;
+					pic[row][column + 1][1] = color_scalar;
+					pic[row][column + 1][2] = color_scalar / 2;
+				}
+				if(row < pixels - 1){
+					pic[row + 1][column][0] = color_scalar;
+					pic[row + 1][column][1] = color_scalar;
+					pic[row + 1][column][2] = color_scalar / 2;
+				}
 			}
 		}
 	}
